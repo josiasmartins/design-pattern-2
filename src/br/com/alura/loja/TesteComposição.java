21fe8ec52b66cbdec67a6b51643ba26e25e176a7
Caitano <josiasmartinscaitano@gmail.com>
2023-06-08 20:53:17 -0300

    feat: add pattern proxy for cache

--- src/br/com/alura/loja/TesteComposição.java
+++ src/br/com/alura/loja/TesteComposição.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.orcamento.OrcamentoProxy;
 
 public class TesteComposição {
 	
@@ -16,7 +17,11 @@ public class TesteComposição {
 		novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 		novo.adicionarItem(antigo);
 		
-		System.out.println(novo.getValor());
+		OrcamentoProxy proxy = new OrcamentoProxy(novo);
+		
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+
 	}
 
 }
